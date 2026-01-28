7b4629f0fe8991e82a91667d6edbfc2d2022c51b
faheemkhan-web <faheemkhanwak@gmail.com>
2026-01-28 18:31:31 +0500

    add this file

---
+++ script.js
@@ -0,0 +1,40 @@
+// Show Hello World in alert box
+alert("Hello World");
+
+// Show Hello World in browser console
+console.log("Hello World");
+
+
+// Using Variables
+
+// var variable
+var studentName = "Faheem Khan";
+
+// let variable
+let courseName = "JavaScript Basics";
+
+// const variable
+const instituteName = "SMIT";
+
+// Batch number
+let batchNumber = 493243;
+
+
+console.log(studentName);
+console.log(courseName);
+console.log(batchNumber);
+
+// // // Display Variable Data in Console
+
+console.log("Student Name:", studentName);
+console.log("Course Name:", courseName);
+console.log("Institute Name:", "SMIT");
+console.log("Batch Number:", 493243);
+
+
+// Display data on webpage using document.write
+document.write("<h2>Student Information</h2>");
+document.write("Name: " + "Faheem khan" + "<br>");
+document.write("Course: " + "web development" + "<br>");
+document.write("Institute: " + "SMIT" + "<br>");
+document.write("Batch: " + 493243 + "<br>");
